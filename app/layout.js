d6cf661e6f856785dd02c20c4963f027c5622305
David smart <112566573+davidgraphix@users.noreply.github.com>
2025-07-25 02:26:11 +0100

    Refactor layout and ProductCard components for improved readability and consistency

--- app/layout.js
+++ app/layout.js
@@ -4,22 +4,20 @@ import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
+  title: "QuickCart - DavidSmart",
   description: "E-Commerce with Next.js ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-        <html lang="en">
-        <body className={`${outfit.className} antialiased text-gray-700`} >
+      <html lang="en">
+        <body className={`${outfit.className} antialiased text-gray-700`}>
           <Toaster />
-          <AppContextProvider>
-            {children}
-          </AppContextProvider>
+          <AppContextProvider>{children}</AppContextProvider>
         </body>
       </html>
     </ClerkProvider>
